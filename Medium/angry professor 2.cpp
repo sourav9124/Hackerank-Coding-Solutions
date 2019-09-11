02fe27fab2ad66a03627efde465eb0d330730ca3
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-09-11 21:18:44 +0530

    solved Successsfully

--- Medium/angry professor 2.cpp
+++ Medium/angry professor 2.cpp
@@ -1,53 +1,26 @@
-#include<iostream>
-#include<algorithm>
+// Author: Sourabh
+#include<bits/stdc++.h>
 using namespace std;
 int main()
-
 {
-	int t;
-	cin>>t;
-	int n,k;
-	int count=0;
+    int t;
+    cin >> t;
+    while(t--){
+    int n, k;
+    cin >> n >> k;
+    int ans =0;
+
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin >> x;
     
-	if(t<=10){
-		
-	
-	while(t--)
-	{
-		 cin>>n>>k;
-		 if(n<=1000 && k<=n)
-		 {
-		 	
-		 
-	
-		 int ar[n];
-		 
-		 
-		 for(int i=0;i<n;i++)
-		 {
-		 		cin>>ar[i];
-		 		if(ar[i]>=-100 && ar[i]<=100)
-		 		{
-		 			
-				 
-      		if(ar[i]<0)
-		 	{
-		 		count++;
-			 }
-		}
-		 }
-		 
-		
-		 if(count>k)
-		 {
-		 	cout<<"NO"<<endl;
-		 }
-		 else
-		 {
-		 	cout<<"YES"<<endl;
-		 }
-		 
-	}
+        if(x<=0)    
+            ans++;
+    }    
+
+    (ans>=k) ? cout<<"NO" : cout<<"YES";
+    cout<<endl;
 }
-}
+return 0;
 }
